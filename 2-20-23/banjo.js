81447b0328f0e91538b5ff7c997f5b54a3a91425
seanmckee <seanmingmckee@gmail.com>
2023-02-20 23:50:49 -0800

    2-20

---
+++ 2-20-23/banjo.js
@@ -0,0 +1,9 @@
+function areYouPlayingBanjo(name) {
+    // Implement me
+    if(name.startsWith("R") || name.startsWith("r")){
+      return name +" plays banjo"
+    }else{
+      return name +" does not play banjo"
+    }
+    return name;
+  }
